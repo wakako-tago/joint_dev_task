1a49c9d66939f67306b7b38861c5f6f1bc2f2089
wakako <osamushi.love877@gmail.com>
2020-08-03 20:28:10 +0900

    Submitting ruby q1-8

--- task.rb
+++ task.rb
@@ -5,11 +5,9 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
  # 以下に回答を記載
-  names.push("斎藤")
-  names.each do |name|
-    p "#{name}"
+  names.<<("斎藤")
+  p names
 
-  end
 
 end
 
@@ -18,7 +16,7 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
-  p array1+array2
+  p array1 + array2
 
 
 end
@@ -35,7 +33,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-  p sports.compact
+  p sports.compact!
   # 以下は変更しないで下さい
   p sports
 end
@@ -45,8 +43,8 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-  p array1.size == 0
-  p array2.size == 0
+  p array1.empty?
+  p array2.empty?
 
 end
 
@@ -54,7 +52,8 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-  p numbers1.map! {|n| n*10}
+  numbers2= numbers1.map! {|n| n*10}
+  p numbers2
 
 end
 
@@ -70,8 +69,9 @@ end
 def q8
   programming_languages = %w(ruby php python javascript)
 
-  # 以下に回答を記載
-  p programming_languages.capitalize
+  # 以下に回答を記載\
+  programming_languages = programming_languages.map{ |p| p.capitalize}
+  upper_case_programming_languages = programming_languages.map{ |p| p.upcase}
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
